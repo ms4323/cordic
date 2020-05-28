7eb28c9aedccf983b5f117945bc30db4824dd5cf
Mahshid Shahmohammadian <ms4323@drexel.edu>
2020-05-28 17:37:15 -0400

    update cordic

--- cordic.hs
+++ cordic.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
 
-import Data.List
+import Data.List as L
 import Data.Bits
 import Data.Sized.Matrix as M
 import Data.Sized.Sampled as SMP
